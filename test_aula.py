cc59c95c57e4b0578470c58c2ef4f191e3cdcf1b
Maristela Weinfurter <maristela.weinfurter@sanar.com>
2021-05-01 22:13:44 -0300

    teste dentro do test_aula.py

--- test_aula.py
+++ test_aula.py
@@ -1,2 +1,19 @@
+import pytest
+
+def soma_1(numero):
+    return int(numero) + 1
+
 def test_soma_1():
     assert soma_1(41) == 42
+
+def test_soma_1_numero_como_string():
+    assert soma_1("41") == 42
+
+def test_soma_1_palavra():
+    with pytest.raises(ValueError):
+        soma_1('mari')
+
+# def test_soma_1_10():
+#     with pytest.raises(ValueError):
+#         soma_1(10)
+#  este teste acima não passa
